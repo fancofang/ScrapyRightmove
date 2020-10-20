3964fe46de56a03d73036b27c6ce4f9ed30bbc5b
fanco <fanghao23@hotmail.com>
2020-10-20 23:32:29 +0100

    init

---
+++ scrapyrightmove/pipelines.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+from pymongo import MongoClient
+import datetime
+
+class ScrapyrightmovePipeline(object):
+
+    collection_name = '12345'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        self.client = MongoClient(self.mongo_uri, connectTimeoutMS=200, retryWrites=True)
+        self.db = self.client[self.mongo_db]
+        self.collection = self.db[self.collection_name]
+        # self.collection.drop()
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        new_item = self.extract_item(item)
+        date = new_item['rent'][0]['date']
+        if self.collection.find_one({'url': new_item['url']}) == None:
+            self.collection.update_one({'url': new_item['url']}, {"$set": dict(new_item)}, upsert=True)
+        elif self.collection.find_one({'url': new_item['url'], 'rent.date': {"$gte": date}}):
+            print("数据库日期已有该日期,更新其他数据")
+            self.collection.update_one({'url': new_item['url']}, {"$set":{'maxprice':new_item['maxprice'],
+                                                                          'minprice':new_item['minprice'],
+                                                                          'letAgreed': new_item['letAgreed'],
+                                                                          'postcode': new_item['postcode'],
+                                                                          'location': new_item['location'],
+                                                                          'addTime': new_item['addTime']
+                                                                          }})
+        else:
+            print("需要更新最新日期数据")
+            self.collection.update_one({'url': new_item['url']}, {"$set":{'maxprice':new_item['maxprice'],
+                                                                          'minprice':new_item['minprice'],
+                                                                          'letAgreed': new_item['letAgreed'],
+                                                                          'postcode': new_item['postcode'],
+                                                                          'location': new_item['location'],
+                                                                          'addTime': new_item['addTime']
+                                                                          },
+                                                                  "$push": {'rent': new_item['rent'][0]}})
+        return new_item
+
+    def extract_item(self, item):
+        # if isinstance(item['rent']['price'],str):       ### price价格为'Infinity'
+        #     item['rent']['price'] = 0
+        # item['rent']['price'] = int(item['rent']['price'])
+        item['rent']['date'] = datetime.datetime.combine(item['rent']['date'], datetime.time())
+        item['maxprice'] = self.maxprice_item(item)
+        item['minprice'] = self.minprice_item(item)
+        # new = item.copy()
+        # del new['rent']
+        temp = []
+        temp.append(item['rent'])
+        item['rent'] = temp
+        return item
+
+
+
+    def maxprice_item(self, item):
+        data = self.collection.find_one({'url':item['url']})
+        if data == None or 'maxprice' not in data:
+            maxprice = item['rent']['price']
+        else:
+            maxprice = max(data['maxprice'], item['rent']['price'])
+        return maxprice
+
+    def minprice_item(self, item):
+        data = self.collection.find_one({'url': item['url']})
+        if data == None or 'minprice' not in data:
+            minprice = item['rent']['price']
+        else:
+            minprice = min(data['minprice'], item['rent']['price'])
+        return minprice
---
+++ scrapyrightmove/spiders/source.py
@@ -0,0 +1,185 @@
+import scrapy
+import re
+import json
+from urllib.parse import urlparse, parse_qs, urlencode
+from scrapyrightmove.items import ScrapyrightmoveItem
+from datetime import datetime, date, time
+import logging
+
+class Myspider(scrapy.Spider):
+    name = 'rightmove'
+
+    start_urls = [
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=1200&minPrice=100'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&includeLetAgreed=true&maxDaysSinceAdded=14',
+
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=1300&minPrice=1200'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&includeLetAgreed=true&maxDaysSinceAdded=14',
+
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=1400&minPrice=1300'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&includeLetAgreed=true&maxDaysSinceAdded=14',
+
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=1500&minPrice=1400'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&sortType=2&includeLetAgreed=true&maxDaysSinceAdded=14',
+
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=1750&minPrice=1500'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&sortType=2&includeLetAgreed=true&maxDaysSinceAdded=7',
+
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=2000&minPrice=1750'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&sortType=2&includeLetAgreed=true&maxDaysSinceAdded=7',
+
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=2500&minPrice=2000'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&sortType=2&includeLetAgreed=true&maxDaysSinceAdded=7',
+
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=3000&minPrice=2500'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&sortType=2&includeLetAgreed=true&maxDaysSinceAdded=14',
+
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=3500&minPrice=3000'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&sortType=2&includeLetAgreed=true&maxDaysSinceAdded=14',
+
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=5000&minPrice=3500'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&sortType=2&includeLetAgreed=true&maxDaysSinceAdded=14',
+
+        'https://www.rightmove.co.uk/property-to-rent/find.html?'
+        'locationIdentifier=REGION%5E87490&minBedrooms=1&maxPrice=20000&minPrice=5000'
+        '&propertyTypes=flat&dontShow=houseShare%2Cretirement&sortType=2&includeLetAgreed=true&maxDaysSinceAdded=14'
+    ]
+
+    custom_settings = {
+        'LOG_LEVEL': 'WARNING'
+    }
+
+    def parse(self, response):
+
+        self.logger.info('Parse function called on %s', response.url)
+        urls = set()
+        htmls = response.xpath('//a[contains(@href,"/property-to-rent/property")]/@href').getall()
+        for i in htmls:
+            urls.add(i)
+            yield response.follow(i, callback=self.parse_details)
+        parsed  = urlparse(response.url)
+        querys = parse_qs(parsed.query)
+        para = {k: v[0] for k, v in querys.items()}
+        if 'index' not in para:
+            para['index'] = '24'
+        else:
+            para['index'] = str(int(para['index']) + 24)
+        data = urlencode(para)
+        next_page = 'https://www.rightmove.co.uk/property-to-rent/find.html?' +data
+        num = response.xpath('//span[@class="searchHeader-resultCount"]/text()').get()
+        num = num.replace(',','')
+        print("总共房源数:",num)
+        print('目前index:', para['index'])
+
+        if int(para['index']) < int(num) :
+            print("准备下一页爬虫")
+            yield response.follow(next_page, callback=self.parse)
+            print(urls)
+        # else:
+        #     raise CloseSpider('完成爬虫,退出!')
+
+    def parse_details(self, response):
+        data = self.extract_details(response)
+        
+        try:
+            id = self.extract_id(data)
+            if id is None:
+                print('Id does not find')
+            
+            title = self.extract_title(data)
+            if title is None:
+                print('Title does not find')
+                
+            address = self.extract_address(data)
+            if address is None:
+                print('Address does not find')
+            
+            agent = self.extract_agentdetails(data)
+            if agent is None:
+                print('Agent does not find')
+                
+            rent = self.extract_rent(data)
+            if rent is None:
+                print('Rent does not find')
+            
+            location = self.extract_location(data)
+            if location is None:
+                print('Location does not find')
+            
+            postcode = self.extract_postcode(data)
+            if postcode is None:
+                print('Postcode does not find')
+                
+            letagreed = self.extract_letagreed(data)
+            if letagreed is None:
+                print('Letagreed does not find')
+                
+            addtime = datetime.combine(date.today(), time(0))
+
+            item = ScrapyrightmoveItem(id=id, url=response.url, agent=agent,
+                                       addTime=addtime, title=title,
+                                       address=address, rent={'date':date.today(),'price':rent},
+                                       letAgreed = letagreed, postcode=postcode, location=location
+            )
+            yield item
+            # print(item)
+        except Exception as e:
+            self.logger.error(str(e))
+
+    def extract_id(self, response):
+        id = response['propertyData']['id']
+        return id
+
+    def extract_title(self, response):
+        title = response['propertyData']['text']['pageTitle']
+        return title
+
+    def extract_address(self, response):
+        address = response['propertyData']['address']['displayAddress']
+        return address
+    
+    def extract_agentdetails(self, response):
+        data = response['propertyData']['customer']['branchDisplayName']
+        agent = data.split(',')[0].strip()
+        return agent
+    
+    def extract_rent(self, response):
+        rent = response['propertyData']['prices']['primaryPrice']
+        price_cursor = re.search(r'([0-9\,]+).+(\w$)', rent, re.M)
+        price = int(price_cursor.group(1).replace(',', ''))
+        if price_cursor.group(2) != 'm':
+            price = round(price * 4.334, 0)
+        rent = int(price)
+        return rent
+    
+    def extract_location(self, response):
+        data = response['propertyData']['location']
+        location = {'latitude': data['latitude'], 'longitude': data['longitude']}
+        return location
+    
+    def extract_postcode(self, response):
+        data = response['propertyData']['propertyUrls']['nearbySoldPropertiesUrl']
+        postcode_cursor = re.search(r'/([\w-]*).html', data)
+        postcode = postcode_cursor.group(1).replace('-', ' ').upper()
+        return postcode
+    
+    def extract_letagreed(self, response):
+        letagreed = response['analyticsInfo']['analyticsProperty']['letAgreed']
+        return letagreed
+
+    def extract_details(self, response):
+        pattern = re.compile(r"window\.PAGE_MODEL = (\{.*})",re.MULTILINE | re.DOTALL)
+        data = response.xpath("//script[contains(., 'window.PAGE_MODEL')]/text()").re(pattern)[0]
+        details = json.loads(data)
+        return details
+        
