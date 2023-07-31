c7a1433f32b223c1e5af760ed4d6dbd6c4086f0a
fanco <fanghao23@hotmail.com>
2023-07-31 13:28:06 +0100

    update update_daily_rent.py content

--- task/update_daily_rent.py
+++ task/update_daily_rent.py
@@ -16,8 +16,13 @@ MYSQL_DB = os.getenv('DB_NAME', 'rightmove')
 
 def get_engine():
     engine = create_engine(
-        'mysql+mysqldb://{user}:{passwd}@{host}/{db}'.format(user=MYSQL_USER, passwd=MYSQL_PASSWD, host=MYSQL_HOST, db=MYSQL_DB))
+        'mysql+mysqldb://{user}:{passwd}@{host}/{db}'.format(user=MYSQL_USER, passwd=MYSQL_PASSWD, host=MYSQL_HOST, db=MYSQL_DB),echo=True, connect_args={'ssl':'DISABLED'})
     return engine
+
+def time_consumption(begin,end):
+    spend = end -begin
+    convert_str = time.strftime("%H:%M:%S", time.gmtime(spend))
+    return convert_str
 
 
 if __name__ == "__main__":
@@ -27,8 +32,12 @@ if __name__ == "__main__":
         "delete from daily_rent "
         "where date < CURRENT_DATE - INTERVAL 2 MONTH"
         )
+    test = text(
+        "select count(*) from daily_rent"
+        )
     with engine.connect() as connection:
         result = connection.execute(stmt)
-        print(result)
-    print("finish ",time.time()-begin)
+        print(result.first())
+    time_spend = time_consumption(begin,time.time())
+    print("Finish! spend time:",time_spend, "and today is:",time.gmtime())
 
